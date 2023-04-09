67248c9497f3474836c4cc4f6ac5dea248b42b50
disvorten <124072944+disvorten@users.noreply.github.com>
2023-04-09 16:44:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import cramervonmises_2samp
+from scipy.stats import anderson_ksamp
 
-chat_id = 1109095907 
+chat_id = 1109095907
 
 
 def solution(x: np.array, y: np.array):
     alpha = 0.1
-    res = cramervonmises_2samp(x, y, method='asymptotic')
+    res = anderson_ksamp([x, y])
     if res.pvalue <= alpha:
         return True
     else:
